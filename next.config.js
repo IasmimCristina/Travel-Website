253edd7f8260e807147ce3ab33a5f993205d8342
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-11-08 15:38:50 -0300

    optimization added.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+}
+// For future deployment
 module.exports = nextConfig
